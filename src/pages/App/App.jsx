304cf5dc9b2b2e68d59b9295f4ec01ed6cfeb0df
Stephanie Viveros <sviveros05@gmail.com>
2021-03-28 14:57:43 -0500

    implement client-side routing and NavBar'

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react';
-import AuthPage from '../AuthPage/AuthPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import './App.css';
+import { useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+// Components
+import AuthPage from "../AuthPage/AuthPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import NavBar from "../../components/NavBar/NavBar";
+
+// Styles
+import "./App.css";
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
   return (
     <main className="App">
-      { user ? <NewOrderPage /> : <AuthPage />}
+      {user ? (
+        <>
+          <NavBar />
+          <Switch>
+            <Route path="/orders/new">
+              <NewOrderPage />
+            </Route>
+            <Route path="/orders">
+              <OrderHistoryPage />
+            </Route>
+            <Redirect to="/orders" />
+          </Switch>
+        </>
+      ) : (
+        <AuthPage />
+      )}
     </main>
   );
 }
-
