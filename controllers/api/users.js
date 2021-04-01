79b7ddfecb493a1a98e6c1a31fd1ce21716aad3c
Stephanie Viveros <sviveros05@gmail.com>
2021-04-01 18:44:55 -0500

    create jwt helper function

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,26 @@
-
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 module.exports = {
-    create
+  create,
 };
 
-function create(req, res){
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    });
+async function create(req, res) {
+  try {
+    // Add user to the db
+    const user = await User.create(req.body);
+  } catch (error) {
+    // Client will check foor non-200 status code
+    // 400 Bad request
+    res.status(400).json(error);
+  }
+}
+
+// Helper Functions
+function createJWT(user) {
+  return jwt.sign(
+    // data payload,
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
 }
--- models/user.js
+++ models/user.js
@@ -1,45 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 6; // variable determines how much processing time it will take to perform the hash.
 
-const userSechema = new Schema({
-  name: { type: String, required: true },
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    required: true,
+
+const userSechema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    trim: true,
-    minLength: 3,
-    required: true,
-  }, {
+  {
     timestamps: true,
     // Even though it's hashed - don't serialize the password
     toJSON: {
-      transform: function(doc, ret) {
+      transform: function (doc, ret) {
         delete ret.password;
         return ret;
-      }
-    }
-  });
+      },
+    },
+  }
+);
 
 // Mongoose pre-save hook that will hash the password anytime the password has changed
-userSechema.pre('save', function(next){
-    // Save the referenc to user doc
-    const user = this;
-    if (!user.isModified('password')) return next();
-    // password has been changed - salt and hash it
-    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash){
-        if (err) return next(err);
-        // Update the password property with the hash
-        user.password = hash;
-        return next();
-    })
-})
+userSechema.pre("save", function (next) {
+  // Save the referenc to user doc
+  const user = this;
+  if (!user.isModified("password")) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // Update the password property with the hash
+    user.password = hash;
+    return next();
+  });
+});
 
 module.exports = mongoose.model("User", userSechema);
